8180b3e1624272f02ba347c57d7824c3f41ec802
freultwah <631910+freultwah@users.noreply.github.com>
2023-11-05 11:27:57 +0200

    PHP 8.2 changes as per ChatGPT recommendations

--- dovecot_impersonate.php
+++ dovecot_impersonate.php
@@ -1,53 +1,56 @@
 <?php
 
 /**
-  * This plugin lets you impersonate another user using a master login. Only works with dovecot.
-  * 
-  * http://wiki.dovecot.org/Authentication/MasterUsers
-  * 
-  * @author Cor Bosman (roundcube@wa.ter.net)
-  */
+ * This plugin lets you impersonate another user using a master login. Only works with dovecot.
+ * 
+ * http://wiki.dovecot.org/Authentication/MasterUsers
+ * 
+ * @author Cor Bosman (roundcube@wa.ter.net)
+ */
   
 class dovecot_impersonate extends rcube_plugin {
-  
-  public function init() 
-  {    
-    $this->add_hook('storage_connect', array($this, 'impersonate'));
-    $this->add_hook('managesieve_connect', array($this, 'impersonate'));
-    $this->add_hook('authenticate', array($this, 'login'));  
-    $this->add_hook('sieverules_connect', array($this, 'impersonate_sieve'));  
-  }
-  
-  function login($data) {
-    // find the seperator character
-    $rcmail = rcmail::get_instance();
-    $this->load_config();
+    private $config;
     
-    $seperator = $rcmail->config->get('dovecot_impersonate_seperator', '*');
+    public function init(): void
+    {    
+        $this->add_hook('storage_connect', [$this, 'impersonate']);
+        $this->add_hook('managesieve_connect', [$this, 'impersonate']);
+        $this->add_hook('authenticate', [$this, 'login']);  
+        $this->add_hook('sieverules_connect', [$this, 'impersonate_sieve']);  
+    }
+  
+    public function login(array $data): array
+    {
+        // find the separator character
+        $rcmail = rcmail::class::get_instance();
+        $this->load_config();
     
-    if(strpos($data['user'], $seperator)) {
-      $arr = explode($seperator, $data['user']);
-      if(count($arr) == 2) {
-        $data['user'] = $arr[0];
-        $_SESSION['plugin.dovecot_impersonate_master'] = $seperator . $arr[1];
-      }
+        $seperator = $rcmail->config->get('dovecot_impersonate_seperator', '*');
+    
+        if (strpos($data['user'], $seperator)) {
+            $arr = explode($seperator, $data['user']);
+            if (count($arr) == 2) {
+                $data['user'] = $arr[0];
+                $_SESSION['plugin.dovecot_impersonate_master'] = $seperator . $arr[1];
+            }
+        }
+        return $data;
     }
-    return($data);
-  }
   
-  function impersonate($data) {
-    if(isset($_SESSION['plugin.dovecot_impersonate_master'])) {
-      $data['user'] = $data['user'] . $_SESSION['plugin.dovecot_impersonate_master']; 
+    public function impersonate(array $data): array
+    {
+        if (isset($_SESSION['plugin.dovecot_impersonate_master'])) {
+            $data['user'] = $data['user'] . $_SESSION['plugin.dovecot_impersonate_master']; 
+        }
+        return $data;
     }
-    return($data);
-  }
   
-  function impersonate_sieve($data) {
-    if(isset($_SESSION['plugin.dovecot_impersonate_master'])) {
-      $data['username'] = $data['username'] . $_SESSION['plugin.dovecot_impersonate_master']; 
+    public function impersonate_sieve(array $data): array
+    {
+        if (isset($_SESSION['plugin.dovecot_impersonate_master'])) {
+            $data['username'] = $data['username'] . $_SESSION['plugin.dovecot_impersonate_master']; 
+        }
+        return $data;
     }
-    return($data);
-  }
-  
 }
-?>
+
